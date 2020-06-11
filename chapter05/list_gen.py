11f1a78b46b3462a8d65f8e4e702bd960570e4ba
niyunsheng <ni-ys13@qq.com>
2020-06-11 21:46:27 +0800

    chapter 5

--- chapter05/list_gen.py
+++ chapter05/list_gen.py
@@ -12,7 +12,7 @@ int_list1 = [1,2]
 int_list2 = [3,4]
 
 qu_list = [(first, second) for first in int_list1 for second in int_list2]
-
+print(qu_list)
 my_dict = {
     "key1":"bobby1",
     "key2":"bobby2"
@@ -29,9 +29,12 @@ int_list = [1,2,3,4,5]
 
 def process_item(item):
     return str(item)
-
+# 字典推导式
 int_dict = {process_item(item):item for item in int_list}
 #列表生成式，第一：能用尽量用， 因为效率高
 print (int_dict)
 
-
+# set推导式
+int_set = {process_item(item) for item in int_list}
+print(type(int_set))
+print(int_set)
--- chapter05/sequence_test.py
+++ chapter05/sequence_test.py
@@ -6,11 +6,11 @@ from collections import abc
 
 a = [1,2]
 c = a + [3,4]
-
+print(c)
 #就地加
 # a += (3,4)
 # 
-# a.extend(range(3))
-
-a.append((1,2))
+a.extend(range(3))
+print(a)
+a.append(range(3))
 print(a)
