c6976f08161aa398a91328654266911d75359441
niyunsheng <ni-ys13@qq.com>
2020-06-15 13:55:16 +0800

    chapter 13

---
+++ chapter13/loop_test0.py
@@ -0,0 +1,20 @@
+#事件循环+回调（驱动生成器）+epoll(IO多路复用)
+#asyncio是python用于解决异步io编程的一整套解决方案
+#tornado、gevent、twisted（scrapy， django channels）
+#torando(实现web服务器)， django+flask(uwsgi, gunicorn+nginx)
+#tornado可以直接部署， nginx+tornado
+
+#使用asyncio
+import asyncio
+import time
+async def get_html(url):
+    print("start get url")
+    await asyncio.sleep(2)
+    print("end get url")
+
+if __name__ == "__main__":
+    start_time = time.time()
+    loop = asyncio.get_event_loop()
+    tasks = [get_html("http://www.imooc.com") for i in range(10)]
+    loop.run_until_complete(asyncio.wait(tasks))
+    print(time.time()-start_time)
